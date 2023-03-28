cd51dc9eeda7fb9055425c67c0c927d85d78d838
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-03-28 12:15:27 +0300

    added images, slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Frederic Rieg';
+export class AppComponent implements OnInit {
+  images = ['books.jpg', 'darmstadt.jpg', 'it.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  /**
+   * OnInit kommt aus der Angular library und wird beim Laden der Seite ausgeführt.
+   */
+  ngOnInit() {
+    this.updateImage();
+  }
+
+
+  updateImage() {
+    setInterval( () => {
+      this.currentImage ++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      },10);
+    }, 8000)
+  }
 }
