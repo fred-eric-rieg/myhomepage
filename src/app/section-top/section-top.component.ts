cfbcbef3f0b100c55dd6d6204c1d3d570883d015
Frederic Rieg <fredericrieg@protonmail.com>
2023-04-26 13:27:06 +0300

    buger menu; blue circle mouse effects

--- src/app/section-top/section-top.component.ts
+++ src/app/section-top/section-top.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-section-top',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./section-top.component.scss']
 })
 export class SectionTopComponent {
+  @ViewChild('blueCircle') blueCircle: any;
 
+  constructor() { }
+
+  /**
+   * Sets the center of the radial gradient to the mouse position.
+   * @param event as mousehover event.
+   */
+  rotate(event: any) {
+    let x = event.clientX;
+    let y = event.clientY;
+    this.blueCircle.nativeElement.style.background = `radial-gradient(74.22% 74.22% at ${x}px ${y}px,  #456990 6.25%, #456990 51.56%, #114B5F 100%)`;
+  }
 }
