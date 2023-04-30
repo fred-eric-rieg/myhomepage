2230576ca0e4ee9f62a2cea407d3b1afc6aaf483
Frederic Rieg <fredericrieg@protonmail.com>
2023-04-30 11:07:00 +0300

    circle animation complete

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mainpage',
@@ -6,49 +6,8 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./mainpage.component.scss']
 })
 export class MainpageComponent {
-  start: number = 0;
-  now: number = 0;
-  t: number = 0;
+  
 
   constructor() { }
 
-  @ViewChild('small') small: any;
-  @ViewChild('smaller') smaller: any;
-
-  isTurning: boolean = false;
-
-  getElement(event: any) {
-    if (!this.isTurning) {
-      this.isTurning = true;
-      this.start = new Date().getTime();
-      this.turnSmall(event);
-    } else {
-      this.isTurning = false;
-    }
-
-  }
-
-  turnSmall(event: any) {
-    setInterval(() => {
-      this.now = new Date().getTime();
-      if (this.now - this.start < 10000) {
-        if (this.t <= 2 * Math.PI) {
-          this.t += Math.PI / 30;
-          event.target.style.left = `calc(50% - 300px * ${Math.cos(this.t)})`;
-          event.target.style.top = `calc(100vh - 30rem - 200px * ${Math.sin(this.t)})`;
-        } else {
-          this.t = 0;
-        }
-        if (event.target.offsetLeft > 294 && this.t < Math.PI) {
-          event.target.style.zIndex = '1';
-        } else {
-          event.target.style.zIndex = '4';
-        }
-      } else {
-        window.clearInterval(0);
-        this.now = 0;
-        this.start = 0;
-      }
-    }, 1000 / 30);
-  }
 }
--- src/app/section-top/section-top.component.ts
+++ src/app/section-top/section-top.component.ts
@@ -1,34 +1,44 @@
-import { Component, HostBinding, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section-top',
   templateUrl: './section-top.component.html',
   styleUrls: ['./section-top.component.scss']
 })
-export class SectionTopComponent implements OnInit {
-  @ViewChild('blueCircle') blueCircle: any;
+export class SectionTopComponent {
+  start: number = 0;
+  now: number = 0;
+  t: number = 0;
 
-  public getScreenWidth: any;
 
 
   constructor() { }
-  ngOnInit(): void {
-    this.getScreenWidth = window.innerWidth;
+
+  getElement(event: any) {
+    this.start = new Date().getTime();
+    this.turnSmall(event);
   }
 
-  /**
-  rotate(event: any) {
-    let x = event.clientX;
-    let y = event.clientY;
-    this.blueCircle.nativeElement.style.background = `radial-gradient(74.22% 74.22% at ${x}px ${y}px,  #456990 6.25%, #456990 51.56%, #114B5F 100%)`;
-  }**/
+  turnSmall(event: any) {
+    setInterval(() => {
+      this.now = new Date().getTime();
+      if (this.t <= 2 * Math.PI) {
+        this.t += Math.PI / 60;
+        if (window.innerWidth < 600) {
+          event.target.style.left = `calc(-250px * ${Math.cos(this.t)})`;
+          event.target.style.top = `calc(-50px * ${Math.sin(this.t)})`;
+        } else {
+          event.target.style.left = `calc(-500px * ${Math.cos(this.t)})`;
+          event.target.style.top = `calc(-100px * ${Math.sin(this.t)})`;
+        }
+      } else {
+        this.t = 0;
+      }
+      if (this.t < Math.PI) {
+        event.target.style.zIndex = '1';
+      } else {
+        event.target.style.zIndex = '4';
+      }
+    }, 1000 / 60);
+  }
 }
-
-
-/**
- * Don't use 100vh for mobile responsive!
- * https://dev.to/nirazanbasnet/dont-use-100vh-for-mobile-responsive-3o97
- * 
- * HostListener window resize
- * https://dev.to/dhanush9952/angular-13-detect-width-and-height-of-screen-tutorial-1of9
- */
