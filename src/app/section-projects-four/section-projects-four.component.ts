bf34803bfcbba13e797ff8404e74763f68a54ab2
Frederic Rieg <fredericrieg@protonmail.com>
2023-05-02 20:03:53 +0300

    light design

--- src/app/section-projects-four/section-projects-four.component.ts
+++ src/app/section-projects-four/section-projects-four.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./section-projects-four.component.scss']
 })
 export class SectionProjectsFourComponent {
+  // Strings are splittet into single letters to animate them.
+  english: string[] = 'Welcome! I am Frederic Rieg'.split('');
+  german: string[] = 'Willkommen! Ich bin Frederic Rieg'.split('');
+  french: string[] = 'Bienvenue! Je suis Frederic Rieg'.split('');
+  dutch: string[] = 'Welkom! Ik ben Frederic Rieg'.split('');
+  turkish: string[] = 'Hoş geldiniz! Ben Frederic Rieg'.split('');
+  japanese: string[] = 'ようこそ！ 私はフレデリック・リーグです'.split('');
+  chinese: string[] = '欢迎你! 我是弗雷德里克-里格'.split('');
+  name: string[] = 'Frederic Rieg:'.split('');
+  profession: string[] = 'Frontend Developer'.split('');
   showBox: boolean = false;
   start: number = 0;
   now: number = 0;
